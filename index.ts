24bc481a308e959d162d7edbd6e9f383999e2017
Hafiza Alina Yasmeen <140942733+alinayasmeen@users.noreply.github.com>
2024-04-23 21:01:50 +0500

    have not changes

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env   node
+//SHABANG
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter your first number", type: "number", name: "firstNum" },
+  { message: "Enter your second number", type: "number", name: "secondNum" },
+  {
+    message: "Select any one of the operator",
+    type: "list",
+    name: "operator",
+    choices: ["Add","Subtract","Multiply","Divide"],
+  },
+]);
+if (answer.operator==="Add") {
+  console.log("your value is "+answer.firstNum+answer.secondNum);
+} else if (answer.operator==="Subtract") {
+  console.log("your value is "+(answer.firstNum-answer.secondNum));
+} else if (answer.operator==="Multiply") {
+  console.log("your value is "+(answer.firstNum*answer.secondNum));
+} else if (answer.operator==="Divide") {
+  console.log("your value is "+(answer.firstNum/answer.secondNum) );
+} else {
+ console.log("Kindly select a suitable oprator");
+}
